fdbef49f1c502081f1fb09d3a0683b3c258b755f
Dwayne Campbell <guerrilla.venture@gmail.com>
2022-08-09 15:20:06 -0700

    Resolved all smart contract paths

--- server/controller/blockchain/token.js
+++ server/controller/blockchain/token.js
@@ -18,8 +18,8 @@ const keyData = "880cc13f65a639ab6ddd37afbfa9b008bc5045fccc2da1b715b67f7c8317fde
 const address = '0xf62b5824d151094359C831A6195112e355D5dC61';
 const eth_id = 'https://kovan.infura.io/v3/68eb211506c141e78162043b7b0df69a';
 
-const XLNToken = require('../../../artifacts/contracts/XLN_Token.sol/XLNToken.json');
-const XLNICO = require('../../../artifacts/contracts/XLN_ICO.sol/XLNICO.json');
+// const XLNToken = require('../../../artifacts/contracts/XLN_Token.sol/XLNToken.json');
+// const XLNICO = require('../../../artifacts/contracts/XLN_ICO.sol/XLNICO.json');
 
 
 // Middleware
@@ -28,72 +28,76 @@ const { checkConditionals, preventPublicKnowledge  } = require('../../middleware
 
 
 const mint  = async ( address, amount ) => {
-  const web3 = new Web3(eth_id)
-  const networkId = await web3.eth.net.getId();
-  XLNToken.networks = [networkId];
-  const xlnToken = new web3.eth.Contract(
-    XLNToken.abi,
-    XLNToken?.networks[networkId]
-  );
+
+res.send('Mint')
+  // const web3 = new Web3(eth_id)
+  // const networkId = await web3.eth.net.getId();
+  // XLNToken.networks = [networkId];
+  // const xlnToken = new web3.eth.Contract(
+  //   XLNToken.abi,
+  //   XLNToken?.networks[networkId]
+  // );
 
 
-  const tx = xlnToken.methods.mint(address, amount );
+  // const tx = xlnToken.methods.mint(address, amount );
 
-  const gas = await tx.estimateGas({ from: address });
-  const gasPrice = await web3.eth.getGasPrice();
-  const data = await tx.encodeABI();
-  const nonce = await web3.eth.getTransactionCount(address);
+  // const gas = await tx.estimateGas({ from: address });
+  // const gasPrice = await web3.eth.getGasPrice();
+  // const data = await tx.encodeABI();
+  // const nonce = await web3.eth.getTransactionCount(address);
 
-  const signedTx = await web3.eth.accounts.signTransaction({
-    to: xlnToken.options.address,
-    data,
-    gas,
-    value: 100 *  10 ** 18,
-    gasPrice,
-    chainId: networkId
-  },
-  keyData
-  );
+  // const signedTx = await web3.eth.accounts.signTransaction({
+  //   to: xlnToken.options.address,
+  //   data,
+  //   gas,
+  //   value: 100 *  10 ** 18,
+  //   gasPrice,
+  //   chainId: networkId
+  // },
+  // keyData
+  // );
 
-  console.log(` Old data value: ${ await xlnToken.methods.data().call() }`);
-  const receipt = await web3.ethers.sendTransaction(signedTx.rawTransaction);
-  console.log(`Transaction hash: ${receipt.transactionHash}`)
-  console.log(` New data value: ${ await xlnToken.methods.data().call() }`);
+  // console.log(` Old data value: ${ await xlnToken.methods.data().call() }`);
+  // const receipt = await web3.ethers.sendTransaction(signedTx.rawTransaction);
+  // console.log(`Transaction hash: ${receipt.transactionHash}`)
+  // console.log(` New data value: ${ await xlnToken.methods.data().call() }`);
 
 }
 
 const updateAdmin  = async ( address ) => {
-  const web3 = new Web3(eth_id)
-  const networkId = await web3.eth.net.getId();
-  XLNToken.networks = [networkId];
-  const xlnToken = new web3.eth.Contract(
-    XLNToken.abi,
-    XLNToken?.networks[networkId]
-  );
+
+  res.send('Update Admin')
+  // const web3 = new Web3(eth_id)
+  // const networkId = await web3.eth.net.getId();
+  // XLNToken.networks = [networkId];
+  // const xlnToken = new web3.eth.Contract(
+  //   XLNToken.abi,
+  //   XLNToken?.networks[networkId]
+  // );
 
 
-  const tx = xlnToken.methods.updateAdmin( address );
+  // const tx = xlnToken.methods.updateAdmin( address );
 
-  const gas = await tx.estimateGas({ from: address });
-  const gasPrice = await web3.eth.getGasPrice();
-  const data = await tx.encodeABI();
-  const nonce = await web3.eth.getTransactionCount(address);
+  // const gas = await tx.estimateGas({ from: address });
+  // const gasPrice = await web3.eth.getGasPrice();
+  // const data = await tx.encodeABI();
+  // const nonce = await web3.eth.getTransactionCount(address);
 
-  const signedTx = await web3.eth.accounts.signTransaction({
-    to: xlnToken.options.address,
-    data,
-    gas,
-    value: 100 *  10 ** 18,
-    gasPrice,
-    chainId: networkId
-  },
-  keyData
-  );
+  // const signedTx = await web3.eth.accounts.signTransaction({
+  //   to: xlnToken.options.address,
+  //   data,
+  //   gas,
+  //   value: 100 *  10 ** 18,
+  //   gasPrice,
+  //   chainId: networkId
+  // },
+  // keyData
+  // );
 
-  console.log(` Old data value: ${ await xlnToken.methods.data().call() }`);
-  const receipt = await web3.ethers.sendTransaction(signedTx.rawTransaction);
-  console.log(`Transaction hash: ${receipt.transactionHash}`)
-  console.log(` New data value: ${ await xlnToken.methods.data().call() }`);
+  // console.log(` Old data value: ${ await xlnToken.methods.data().call() }`);
+  // const receipt = await web3.ethers.sendTransaction(signedTx.rawTransaction);
+  // console.log(`Transaction hash: ${receipt.transactionHash}`)
+  // console.log(` New data value: ${ await xlnToken.methods.data().call() }`);
 
 }
 
