105332850491788c741c02bc2152ed63aadb419e
Medallion XLN <56490878+medallion-xln@users.noreply.github.com>
2022-09-15 16:51:41 -0700

    Delete server/config directory

--- server/config/config.js
+++
@@ -1,7 +0,0 @@
-exports.tokenPrice = 0.07;
-exports.deployerAddress = '0x75cFC852f0A83DF3b19ead24793240B37b7DfadD'
-exports.tokenAddress = '0xcbEAF3BDe82155F56486Fb5a1072cb8baAf547cc'
-exports.icoAddress = '0x1429859428C0aBc9C2C47C8Ee9FBaf82cFA0F20f'
-exports.nftAddress = '0xFD471836031dc5108809D173A067e8486B9047A3'
-exports.nftMarketAddress = '0xc351628EB244ec633d5f21fBD6621e1a683B1181'
-          
