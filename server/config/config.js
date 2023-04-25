b35ffee201e61e5fef40ca4c65b0e7ab1d738c20
Dwayne Campbell <129335704+dcampbell-hack@users.noreply.github.com>
2023-04-25 11:03:38 -0700

    Delete config.js

--- server/config/config.js
+++
@@ -1,7 +0,0 @@
-export const tokenPrice = 0.07;
-export const deployerAddress = '0x75cFC852f0A83DF3b19ead24793240B37b7DfadD'
-export const tokenAddress = '0xcbEAF3BDe82155F56486Fb5a1072cb8baAf547cc'
-export const icoAddress = '0x1429859428C0aBc9C2C47C8Ee9FBaf82cFA0F20f'
-export const nftAddress = '0xFD471836031dc5108809D173A067e8486B9047A3'
-export const nftMarketAddress = '0xc351628EB244ec633d5f21fBD6621e1a683B1181'
-          
